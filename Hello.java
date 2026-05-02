507f43be1ac67061a2b40efb6f58b2ea03b0781b
Tushar Tanwar <146130670+TRrajputDEV@users.noreply.github.com>
2026-05-02 23:11:57 +0530

    Add Hello.java class with a simple print method and main execution

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello {
+
+    public void func(){
+        System.out.print("Hello World !!");
+    }
+    public static void main(String[] args) {
+        Hello obj = new Hello();
+        obj.func();
+
+    }
+}
+
+/*
+Java is stack based execution - in jvm
+*/
