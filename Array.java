f335af8ae4337187ab0054d8e759db2d1a963689
Tushar Tanwar <146130670+TRrajputDEV@users.noreply.github.com>
2026-05-02 23:32:38 +0530

    Add Array and Student classes with basic functionality

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        Student[] arr;
+
+        arr = new Student[5];
+
+        arr[0] = new Student(1, "Tushar");
+
+        System.out.print(arr[0]);
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    public int roll_no;
+    public String name;
+
+    Student(int roll_no, String name) {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+
+    public String toString(){
+        return roll_no + " "  + name;
+    }
+
+}
